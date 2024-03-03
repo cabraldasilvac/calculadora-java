ac80a1a4dd1da6d5bd4230ef1df0bd6bdb950eeb
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-03-03 11:48:32 -0300

    :rocket: create Getters and Setters methods

--- src/poo/Calculadora.java
+++ src/poo/Calculadora.java
@@ -3,4 +3,13 @@ package poo;
 public class Calculadora {
   private Double x; // Retorna o resultado
 
+  // Generate Getters e Setters
+  public Double getX() {
+    return x;
+  }
+
+  public void setX(Double x) {
+    this.x = x;
+  }
+
 }
